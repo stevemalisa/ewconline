8c14d61c42bd7a327a2bb31721f310d060f41a1b
Emelio Steve Malisa <emeliom@posbang.com>
2018-09-08 22:51:19 +0800

    Adding login function

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,7 @@
 <div class="log-w3">
 <div class="w3layouts-main">
     <h2>ADMIN</h2>
-        <form action="login_verify.php" method="post">
+        <form action="{{  url(/loginverify) }}" method="post">
             <input type="username" class="ggg" name="username" id="username" placeholder="USERNAME" required="">
             <input type="password" class="ggg" name="password" id="password" placeholder="PASSWORD" required="">
 
